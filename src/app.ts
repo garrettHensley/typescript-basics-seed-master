c186a730dfebe8cffa2b90ec2c66d7e326cae800
Garrett Hensley <garrettmichaelhensley@gmail.com>
2021-10-17 17:45:02 -0400

    new changes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('Ahoyy TypeScript!');
+const pizzas = [
+    { name: 'Pepporoni', toppings: ['pepporoni'] },
+]
+
+const mappedPizzas = pizzas.map((pizza) => pizza.name.toUpperCase())
+
+console.log(mappedPizzas)
+
+const pizza = {
+    name: "Blazing Inferno",
+    getName: function () { console.log(this.name) }
+}
+
+
+console.log(pizza.getName())
+pizza.getName()
